af632d406b85412218abfa7dc2087ebaeefcdc5b
Lindson França <l1nds0n@hotmail.com>
2019-10-04 11:05:45 -0300

    Comportamentos comuns plenos

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom'
+import api from '../../services/api';
 
-export default function Dash(){
-    return <h1>Dashboard!</h1>
+import './style.css';
+
+export default function Dashboard(){
+    const [spots, setSpots] = useState([]);
+    useEffect(() => {
+        async function loadSpots(){
+            const user_id = localStorage.getItem('user');
+            const response = await api.get("/dashboard", {
+                headers: { user_id }
+            });
+            setSpots(response.data);
+        }
+        loadSpots();
+    }, [])
+    return (
+        <>
+        <ul className="spot-list">
+            {spots.map(spot => (
+                <li key={spot._id}>
+                    <header style={{ backgroundImage: `url(${ spot.thumbnail_url })`}}></header>
+                    <strong>{spot.company}</strong>
+                    <span>{spot.price ? `R$${spot.price}/dia` : 'Gratuíto'}</span>
+                </li>
+            ))}
+        </ul>
+        <Link to="/new">
+            <button className="btn">
+            Cadastrar novo spot
+            </button>
+        </Link>
+        </>
+    )
 }
